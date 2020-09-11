668dbc0161c294bc7bd6e3c1c0cce48f52000dfd
sujiaheng <648952316@qq.com>
2020-09-11 14:25:01 +0800

    feat(ruby): add ruby support

---
+++ ruby/README.md
@@ -0,0 +1,52 @@
+## Ruby
+
+> 需要注意的是，此实现并没有发到 `gem` 上，需使用方自行复制
+
+### 使用实例
+
+#### SDK Token
+
+```ruby
+require './lib/token.rb'
+
+NetlessToken.sdk_token(
+    "netless ak",
+    "netless sk",
+    1000 * 60 * 10, # token 有效时间 (10分钟)，为 0 时，即永不过期。单位毫秒
+    {
+        :role => NetlessToken::ROLE::ADMIN  # 可以选择 NetlessToken::ROLE::WRITER / NetlessToken::ROLE::READER 
+    }
+)
+```
+
+#### Room Token
+
+```ruby
+require './lib/token.rb'
+
+NetlessToken.room_token(
+    "netless ak",
+    "netless sk",
+    1000 * 60 * 10, # token 有效时间 (10分钟)，为 0 时，即永不过期。单位毫秒
+    {
+        :role => NetlessToken::ROLE::READER  # 可以选择 NetlessToken::ROLE::WRITER / NetlessToken::ROLE::ADMIN 
+        :uuid => "房间的 UUID"
+    }
+)
+```
+
+#### Task Token
+
+```ruby
+require './lib/token.rb'
+
+NetlessToken.task_token(
+    "netless ak",
+    "netless sk",
+    1000 * 60 * 10, # token 有效时间 (10分钟)，为 0 时，即永不过期。单位毫秒
+    {
+        :role => NetlessToken::ROLE::READER  # 可以选择 NetlessToken::ROLE::WRITER / NetlessToken::ROLE::ADMIN 
+        :uuid => "房间的 UUID"
+    }
+)
+```
---
+++ ruby/lib/token.rb
@@ -0,0 +1,49 @@
+require "json"
+require "uuidtools"
+require "openssl"
+require "base64"
+
+module NetlessToken
+
+  module ROLE
+    ADMIN = "0"
+    WRITER = "1"
+    READER = "2"
+  end
+    
+  module PREFIX
+    SDK = "NETLESSSDK_"
+    ROOM = "NETLESSROOM_"
+    TASK = "NETLESSTASK_"
+  end
+
+  def create_token(prefix, access_key, secret_access_key, lifespan, content)
+    object = {
+      ak: access_key,
+      nonce: UUIDTools::UUID.timestamp_create.to_s
+    }
+    object.merge!(content)
+      
+    if lifespan > 0
+      object.store(:expireAt, ((Time.now.to_f * 1000).to_i + lifespan).to_s)
+    end
+
+    infomation = object.sort.to_h.to_json
+    digest = OpenSSL::Digest.new('sha256')
+    hmac = OpenSSL::HMAC.hexdigest(digest, secret_access_key, infomation)
+    object.store(:sig, hmac)
+    prefix + Base64.urlsafe_encode64(URI.encode_www_form(object.sort.to_h)).gsub(/=+$/, "")
+  end
+
+  def self.sdk_token *args
+    create_token PREFIX::SDK, *args
+  end
+
+  def self.room_token *args
+    create_token PREFIX::ROOM, *args
+  end
+
+  def self.task_token *args
+    create_token PREFIX::TASK, *args
+  end
+end
